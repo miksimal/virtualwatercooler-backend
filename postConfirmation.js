a035a93a44d294abcb80f34914f2a2dc0a8b4495
Mikkel Lauritzen <mikkel.hlauritzen@gmail.com>
2020-06-20 13:06:34 +0100

    Fix postConfirmation, hardcoded email send

---
+++ postConfirmation.js
@@ -0,0 +1,25 @@
+import AWS from "aws-sdk";
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+export function main(event, context, callback) {
+  const params = {
+    TableName: process.env.USERS_TABLE,
+    Item: {
+      organisationId: event.request.userAttributes['custom:organisationId'],
+      organisationName: event.request.userAttributes['custom:organisationName'],
+      userId: event.userName,
+      email: event.request.userAttributes.email,
+      firstName: event.request.userAttributes.name,
+      createdAt: Date.now()
+    }
+  };
+
+  dynamoDb.put(params, (error) => {
+    if (error) {
+      console.log(error);
+      return;
+    }
+    callback(null, event);
+  });
+}
