56f55b58130616fe61a36baf3461472ec92b5792
Mikkel Lauritzen <mikkel.hlauritzen@gmail.com>
2020-06-22 22:03:31 +0100

    Ugly but works - employees get orgid and orgname

--- add.js
+++ add.js
@@ -2,45 +2,68 @@ import * as uuid from "uuid";
 import AWS from "aws-sdk";
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const cognitoidentityserviceprovider = new AWS.CognitoIdentityServiceProvider();
 
 export function main(event, context, callback) {
-  const data = JSON.parse(event.body);
+  const addUserData = JSON.parse(event.body);
+  const authProvider = event.requestContext.identity.cognitoAuthenticationProvider;
+  const parts = authProvider.split(':');
+  const userPoolUserId = parts[parts.length-1];
+  // note can also get the userpoolid from there, if I want.
 
-  const params = {
-    TableName: process.env.USERS_TABLE,
-    Item: {
-      organisationId: data.organisationId,
-      userId: uuid.v1(),
-      email: data.email,
-      firstName: data.firstName,
-      createdAt: Date.now()
-    }
+  var orgId;
+  var orgName;
+
+  var getUserParams = {
+    UserPoolId: process.env.USER_POOL_ID,
+    Username: userPoolUserId
   };
+  cognitoidentityserviceprovider.adminGetUser(getUserParams, function(err, data) {
+    if (err) console.log(err, err.stack);
+    else {
+      orgId = data.UserAttributes.filter(function(attribute) { return attribute.Name === 'custom:organisationId'; })[0].Value;
+      orgName = data.UserAttributes.filter(function(attribute) { return attribute.Name === 'custom:organisationName'; })[0].Value;
 
-  dynamoDb.put(params, (error, data) => {
-    // Set response headers to enable CORS (Cross-Origin Resource Sharing)
-    const headers = {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Credentials": true
-    };
-
-    // Return status code 500 on error
-    if (error) {
-      const response = {
-        statusCode: 500,
-        headers: headers,
-        body: JSON.stringify({ status: false })
+      const params = {
+        TableName: process.env.USERS_TABLE,
+        Item: {
+          organisationId: orgId,
+          organisationName: orgName,
+          userId: uuid.v1(),
+          email: addUserData.email,
+          firstName: addUserData.firstName,
+          createdAt: Date.now()
+        }
       };
-      callback(null, response);
-      return;
-    }
 
-    // Return status code 200 and the newly created item
-    const response = {
-      statusCode: 200,
-      headers: headers,
-      body: JSON.stringify(params.Item)
-    };
-    callback(null, response);
+      console.log(JSON.stringify(params.Item, null, 2));
+
+      dynamoDb.put(params, (error) => {
+        // Set response headers to enable CORS (Cross-Origin Resource Sharing)
+        const headers = {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Credentials": true
+        };
+
+        // Return status code 500 on error
+        if (error) {
+          const response = {
+            statusCode: 500,
+            headers: headers,
+            body: JSON.stringify({ status: false })
+          };
+          callback(null, response);
+          return;
+        }
+
+        // Return status code 200 and the newly created item
+        const response = {
+          statusCode: 200,
+          headers: headers,
+          body: JSON.stringify(params.Item)
+        };
+        callback(null, response);
+      });
+    }
   });
 }
