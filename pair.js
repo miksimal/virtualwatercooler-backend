68fab58e8198503b299c217342fad233c41fe4fc
Mikkel Lauritzen <mikkel.hlauritzen@gmail.com>
2020-08-28 09:32:06 +0100

    factor out common logic for emailsending and pairing

--- pair.js
+++ pair.js
@@ -1,4 +1,5 @@
 import AWS from "aws-sdk";
+import pair from "./libs/pair-lib";
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
@@ -31,29 +32,7 @@ export function main(event, context, callback) {
       return;
     }
 
-    let shuffledArray = data.Items.map(e => ({email: e.email, firstName: e.firstName, organisationName: e.organisationName}));
-
-    for(let i = shuffledArray.length-1; i > 0; i--){
-      const j = Math.floor(Math.random() * i);
-      const temp = shuffledArray[i];
-      shuffledArray[i] = shuffledArray[j];
-      shuffledArray[j] = temp;
-    }
-
-    let pairs = [];
-    let i = shuffledArray.length-1;
-    while(i>=0) {
-      if(i>=1){
-        let pair = [shuffledArray[i], shuffledArray[i-1]];
-        pairs.push(pair);
-        i = i -2;
-      }
-      else if(i==0){
-        let pair = [shuffledArray[i], shuffledArray[i+1]];
-        pairs.push(pair);
-        i--;
-      }
-    }
+    let pairs = pair(data);
 
     const response = {
       statusCode: 200,
